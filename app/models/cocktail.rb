ad805d2029233f92f2a0aec09a00519e64d3af2e
Louis Fialho <louis.fialho@hec.edu>
2020-08-21 12:24:47 +0100

    added cloudinary

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,8 +1,11 @@
 class Cocktail < ApplicationRecord
+
+  has_one_attached :photo
+
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
 
   validates :name, presence: true
   validates :name, uniqueness: true
-  validates :image_url, presence: true
+  validates :photo, presence: true
 end
